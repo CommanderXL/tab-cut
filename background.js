236d6b2807e983cd264650d3da775880217b867f
XRene <1017653702@qq.com>
2017-05-31 11:05:46 +0800

    update

---
+++ background.js
@@ -0,0 +1,4 @@
+chrome.tabs.getAllInWindow((tabsArr) => {
+  console.log(tabsArr)
+})
+console.log(123)
--- common/util.js
+++ common/util.js
@@ -1,3 +1,10 @@
 function util () {
-  window.alert(123)
+  chrome.tabs.getAllInWindow((arrTabs) => {
+    // window.alert(arrTabs.length)
+  })
+  console.log(123)
+  /* chrome.windows.create({
+    url: 'http://www.baidu.com',
+    type: 'popup'
+  }) */
 }
